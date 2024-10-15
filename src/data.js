937ae86619d1dd93cbb20de5c97ca82dd04b6809
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-16 00:42:22 +0500

    add data.js

---
+++ src/data.js
@@ -0,0 +1,274 @@
+const products = [
+  {
+    id: "1",
+    Title:
+      "Electric Fireplace Heater Portable Electric Fireplace Heater Indoor,Beige",
+    Color: "Beige",
+    Price: "189.99$",
+    Brand: "Xbeauty",
+    PowerSource: "Battery Powered",
+    ProductDimensions: '10.7"D x 23.5"W x 24.3"H',
+    Material: "Stainless Steel",
+    FinishType: "Painted",
+    InstallationType: "Freestanding",
+    Heat_Output: "5100 British Thermal Units",
+    Special_Feature: "Led Flame",
+    Style: "Classic",
+    // Room_Type: "",
+    // Ventilation_Type: "",
+    detail: [
+      [
+        "Vintage Fireplace:Classic retro style,original medieval church gate design, Strong Steel Structure. When the cold sets in, there no better feelithan relaxing by your fireplace, whose efficient 5,100 BTU heat output keeps you warm all winter.",
+      ],
+      [
+        "Portable fireplace: It is only 23.5in*10.7in*24.3in, which means its very portable. Adults can move easily.The electric fireplace heater suitable for many occasions, whether it is a office, a restaurant or a family.",
+      ],
+      [
+        "Realistic Flame Vision:The fire effect of the fireplace heater aesthetically surpasses real flames,the atmosphere created by the combination of bright flames and logs is unparalleled, 3 transparent glass lets you see the realistic flame effect from any angle,so enjoy the visual experience and warmth of this fireplace stove.",
+      ],
+      [
+        "Flame Speed & Brightness are Adjusted:Flame effects can be separated from heat;The flame effect can be controlled independently without heating;Four color-changing flames that change depending on your mood,and the flame dimmer controls the brightness of the flame effect to help you set up when you do not need a heater.",
+      ],
+      [
+        "Safe and Overheat Protection:When the temperature reaches a critical level, the fireplace's overheating protection system will automatically turn off the heater to ensure the user's safety. Furnace bottom design air outlet, can effectively avoid the fuselage overheating and high temperature harm.",
+      ],
+      [
+        "Safe and Overheat Protection:When the temperature reaches a critical level, the fireplace's overheating protection system will automatically turn off the heater to ensure the user's safety. Furnace bottom design air outlet, can effectively avoid the fuselage overheating and high temperature harm.",
+      ],
+    ],
+    Images: [
+      "https://m.media-amazon.com/images/I/71qKr6JfNFL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71TCksDyOOL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71iz0CvkQPL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/810CMweeJzL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71J5kRWhBvL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71wI6BxvaLL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81kI5whJIfL._AC_SL1500_.jpg",
+    ],
+  },
+  {
+    id: "2",
+    Title:
+      " Electric Fireplace Heater Portable Electric Fireplace Heater Indoor,Black",
+    Color: " Black",
+    Price: "189.99$",
+    Brand: "Xbeauty",
+    PowerSource: "Electric",
+    ProductDimensions: '10"D x 23"W x 24"H',
+    Material: "Stainless Steel",
+
+    InstallationType: "Freestanding",
+    Heat_Output: "5100 British Thermal Units",
+    Special_Feature:
+      "Led Flame, Multicolor Flame, Timer function, Adjustable Flame, Two-stage Temperature Control",
+    Style: "Classic",
+    Room_Type: "Office, Kitchen",
+
+    detail: [
+      [
+        "Vintage Fireplace:Classic retro style,original medieval church gate design, Strong Steel Structure. When the cold sets in, there no better feelithan relaxing by your fireplace, whose efficient 5,100 BTU heat output keeps you warm all winter.",
+      ],
+      [
+        "Portable fireplace: It is only 23.5in*10.7in*24.3in, which means its very portable. Adults can move easily.The electric fireplace heater suitable for many occasions, whether it is a office, a restaurant or a family.",
+      ],
+      [
+        "Realistic Flame Vision:The fire effect of the fireplace heater aesthetically surpasses real flames,the atmosphere created by the combination of bright flames and logs is unparalleled, 3 transparent glass lets you see the realistic flame effect from any angle,so enjoy the visual experience and warmth of this fireplace stove.",
+      ],
+      [
+        "Flame Speed & Brightness are Adjusted:Flame effects can be separated from heat;The flame effect can be controlled independently without heating;Four color-changing flames that change depending on your mood,and the flame dimmer controls the brightness of the flame effect to help you set up when you do not need a heater.",
+      ],
+      [
+        "Safe and Overheat Protection:When the temperature reaches a critical level, the fireplace's overheating protection system will automatically turn off the heater to ensure the user's safety. Furnace bottom design air outlet, can effectively avoid the fuselage overheating and high temperature harm.",
+      ],
+      [
+        "Two-stage Temperature Control: Available in two temperature Settings, 500W and 1500W. In addition, the fireplace heater has a timing function so that you can save resources and have a warm room when you get home.",
+      ],
+    ],
+    Images: [
+      "https://m.media-amazon.com/images/I/71fA-Ut1G2L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81PYoo0ErCL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71wI6BxvaLL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81Unu0Ex94L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71khYLq7d0L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81aO3ulDoDL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71ohuJFi38L._AC_SL1500_.jpg",
+    ],
+  },
+  {
+    id: "3",
+    Title:
+      "Xbeauty Electric Fireplace Stove, Freestanding Fireplace Heater with Realistic Flame, Indoor Electric Stove Heater, Portable, Infrared, Thermostat, Overheating Safety System, 1000W/1500W(23 Inch)",
+    Color: "Black-23inch",
+    Price: "149.99$",
+    Brand: "Xbeauty",
+    PowerSource: "Corded Electric",
+    ProductDimensions: '12.6"D x 23.6"W x 22.5"H',
+
+    InstallationType: "Freestanding",
+    Heat_Output: "5100",
+    Special_Feature: "Portable",
+    Style: "Portable,Retro,Classic,Unique",
+    Room_Type: "Study, Bedroom, Living Room",
+    Ventilation_Type: "Vent Free",
+    detail: [
+      [
+        "[Grace beautiful Infrared heater] Elegant and unique design, classic retro style, and stable solid structure. These words do not even begin to describe this electric fireplace stove. The designer used special materials to keep the product strong and reduce the weight of the furnace to only  Z6 L X 12.6 W X 22.5 H ,which means the product is very portable. An adult can move it easily. Also, lighter weight means it’s safer.",
+      ],
+      [
+        " [Real and bright flame effect] Based on the latest technology, the flame’s effect of this product not only surpasses the real flame in terms of aesthetics, but also avoids the danger posed by the real flame. The atmosphere created by the combination of the bright flame and the log is unparalleled. You can enjoy the visual experience and warmth brought by this heater in your bedroom, study, hall with your family.",
+      ],
+      [
+        "[Safe and efficient power] This electric stove heater has two heating modes of 1000W and 1500W to adapt to different environments and needs. 120V voltage, 60Hz frequency, 5100BTUS, 12.5AMPS. The overheating protection system will shut down the power when the temperature reaches a critical level to ensure the safety of user. In the lower right corner of the furnace has knobs for adjusting the thermostat and brightness of the flame, which is very convenient and relaxed",
+      ],
+      [
+        " [Warm and comfortable experience] This product USES the latest infrared heating technology to make heating more safer. Moreover, after testing, the maximum heating range of this heater is 400sq to 500sq, and the average heating time is 5 to 7 minutes. The vents at the bottom of the furnace effectively reduce the noise generated by the heater when it is working. The maximum noise will not exceed 40db.",
+      ],
+      [
+        " When the cold sets in, there's no better feeling than relaxing by your fireplace, whose efficient heat output keeps you warm all winter.",
+      ],
+    ],
+    Images: [
+      "https://m.media-amazon.com/images/I/7126zgAWrxL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81hEC6BoHkL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71NhRtBxUfL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/711PGgD7L7L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71jlLcnq8AL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/719p1-vT71L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71LzBVcN8mL._AC_SL1500_.jpg",
+    ],
+  },
+  {
+    id: "4",
+    Title:
+      "Electric Fireplace Heater Portable Electric Fireplace Stove Heater Indoor,12 Flame Colors,12 Flame Bed Colors,5 Flame Brightness,5 Flame Speeds,Control The Temperature",
+    Color: "Black-24inch",
+    Price: "199.99$",
+    Brand: "Xbeauty",
+    PowerSource: "Corded Electric",
+    ProductDimensions: '10.67"D x 23.46"W x 24.25"H',
+    Material: "Stainless Steel",
+    FinishType: "Stainless Steel",
+    InstallationType: "Freestanding",
+    Heat_Output: "5100 British Thermal Units",
+    Special_Feature: "Portable, Flame Effect",
+    Style: "Colonial",
+
+    detail: [
+      [
+        "Vintage Fireplace:Classic retro style,original medieval church gate design, Strong Steel Structure. When the cold sets in, there's no better feeling than relaxing by your fireplace, whose efficient 5,100 BTU heat output keeps you warm all winter",
+      ],
+      [
+        "Portable Fireplace: It's only 23.46in*10.67in*24.25in, which means it's very portable. Adults can move easily.The electric fireplace heater suitable for many occasions, whether it is a office, a restaurant or a family.",
+      ],
+      [
+        " Realistic Flame Vision:The fire effect of the fireplace heater aesthetically surpasses real flames,the atmosphere created by the combination of bright flames and logs is unparalleled, 3 transparent glass lets you see the realistic flame effect from any angle,so enjoy the visual experience and warmth of this fireplace stove",
+      ],
+      [
+        " Flame Speed & Brightness are Adjusted:Flame effects can be separated from heat;The flame effect can be controlled independently without heating;Four color-changing flames that change depending on your mood,and the flame dimmer controls the brightness of the flame effect to help you set up when you do not need a heater.",
+      ],
+      [
+        " Safe and Overheat Protection:When the temperature reaches a critical level, the fireplace's overheating protection system will automatically turn off the heater to ensure the user's safety. Furnace bottom design air outlet, can effectively avoid the fuselage overheating and high temperature harm.",
+      ],
+      [
+        "Two-stage Temperature Control: Available in two temperature Settings, 500W and 1500W.In addition, the fireplace heater has a timing function so that you can save resources and have a warm room when you get home.",
+      ],
+    ],
+    Images: [
+      "https://m.media-amazon.com/images/I/81KE+K-CWeL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/711KQsmyxnL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71tworL4csL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/716ZDCGA6AL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81fPAjbBp0L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71fCf35XziL._AC_SL1500_.jpg",
+    ],
+  },
+  {
+    id: "5",
+    Title:
+      "Electric Fireplace Heater Portable Electric Fireplace Heater Indoor,Dark Green",
+    Color: " Dark Green",
+    Price: "189.99$",
+    Brand: "Xbeauty",
+    PowerSource: "Corded Electric",
+    ProductDimensions: '10.7"D x 23.5"W x 24.32"H',
+    Material: "Alloy Steel, Glass",
+    FinishType: "Stainless Steel",
+    InstallationType: "Freestanding",
+    Heat_Output: "5100 British Thermal Units",
+    Special_Feature: "Portable",
+    Style: "Vintage",
+    Room_Type: "	Office, Kitchen",
+
+    detail: [
+      [
+        "Vintage Fireplace:Classic retro style,original medieval church gate design, Strong Steel Structure. When the cold sets in, there's no better feeling than relaxing by your fireplace, whose efficient 5,100 BTU heat output keeps you warm all winter.",
+      ],
+      [
+        "Portable Fireplace: It's only 23.46in*10.67in*24.25in, which means it's very portable. Adults can move easily.The electric fireplace heater suitable for many occasions, whether it is a office, a restaurant or a family.",
+      ],
+      [
+        " Realistic Flame Vision:The fire effect of the fireplace heater aesthetically surpasses real flames,the atmosphere created by the combination of bright flames and logs is unparalleled, 3 transparent glass lets you see the realistic flame effect from any angle,so enjoy the visual experience and warmth of this fireplace stove",
+      ],
+      [
+        " Flame Speed & Brightness are Adjusted:Flame effects can be separated from heat;The flame effect can be controlled independently without heating;Four color-changing flames that change depending on your mood,and the flame dimmer controls the brightness of the flame effect to help you set up when you do not need a heater.",
+      ],
+      [
+        " Safe and Overheat Protection:When the temperature reaches a critical level, the fireplace's overheating protection system will automatically turn off the heater to ensure the user's safety. Furnace bottom design air outlet, can effectively avoid the fuselage overheating and high temperature harm.",
+      ],
+    ],
+    Images: [
+      "https://m.media-amazon.com/images/I/71k53tPxvjL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71UsmHN9kNL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71wI6BxvaLL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71kVrKi7gXL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71BJskqQw9L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81mTDN71OAL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81RfKm0cN0L._AC_SL1500_.jpg",
+    ],
+  },
+  {
+    id: "6",
+    Title:
+      "Electric Fireplace Heater Portable Electric Fireplace Heater Indoor,Gray",
+    Color: "Gray",
+    Price: "189.99$",
+    Brand: "Xbeauty",
+    PowerSource: "Corded Electric",
+    ProductDimensions: '10.7"D x 23.5"W x 24.3"H',
+    Material: "Alloy Steel, Glass",
+    FinishType: "",
+    InstallationType: "Freestanding",
+    Heat_Output: "5100 British Thermal Units",
+    Special_Feature: "Multicolor Flame, Portable, Adjustable Flame",
+    Style: "Classic",
+    Room_Type: "Office, Kitchen",
+
+    detail: [
+      [
+        "Vintage Fireplace:Classic retro style,original medieval church gate design, Strong Steel Structure. When the cold sets in, there's no better feeling than relaxing by your fireplace, whose efficient 5,100 BTU heat output keeps you warm all winter.",
+      ],
+      [
+        "Portable Fireplace: It's only 23.46in*10.67in*24.25in, which means it's very portable. Adults can move easily.The electric fireplace heater suitable for many occasions, whether it is a office, a restaurant or a family.",
+      ],
+      [
+        " Realistic Flame Vision:The fire effect of the fireplace heater aesthetically surpasses real flames,the atmosphere created by the combination of bright flames and logs is unparalleled, 3 transparent glass lets you see the realistic flame effect from any angle,so enjoy the visual experience and warmth of this fireplace stove",
+      ],
+      [
+        " Flame Speed & Brightness are Adjusted:Flame effects can be separated from heat;The flame effect can be controlled independently without heating;Four color-changing flames that change depending on your mood,and the flame dimmer controls the brightness of the flame effect to help you set up when you do not need a heater.",
+      ],
+      [
+        " Safe and Overheat Protection:When the temperature reaches a critical level, the fireplace's overheating protection system will automatically turn off the heater to ensure the user's safety. Furnace bottom design air outlet, can effectively avoid the fuselage overheating and high temperature harm.",
+      ],
+    ],
+    Images: [
+      "https://m.media-amazon.com/images/I/81TvopRtZjL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/8133JGreCUL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81BLu75RNjL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71KEQjcutDL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71mjeK2ZXZL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81kdCtPUz7L._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/71wI6BxvaLL._AC_SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/8134lonZzBL._AC_SL1500_.jpg",
+    ],
+  },
+];
