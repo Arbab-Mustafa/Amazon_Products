b0afd6ff3e111fd251862ca49f99a65ddb151a23
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-25 14:58:21 +0500

    fix the issue on our Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div className="bg-[#F2F3FA] h-screen">
+    <div className="bg-[#F2F3FA]">
       <Header />
       <Banner />
 
